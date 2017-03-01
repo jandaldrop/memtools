c21bdeb10b1c868a7713d381974e908afc797c01
Jan Daldrop <daldrop@zedat.fu-berlin.de>
2017-03-01 19:17:32 +0100

    updated version number

--- memtools.py
+++ memtools.py
@@ -8,7 +8,7 @@ from correlation import *
 
 
 def ver():
-    print("This is memtools version 0.63")
+    print("This is memtools version 0.64")
 
 
 def xframe(x,time,round_time=1.e-4,fix_time=True,dt=-1):
